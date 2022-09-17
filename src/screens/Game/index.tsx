dc90ff844de175bb3935cccd4a4c8d360d30abe6
Luis Murilo <luiszkm@gmail.com>
2022-09-17 15:52:01 -0300

    add Duo component

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,12 +1,56 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Background } from '../../components/Background';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { Entypo } from '@expo/vector-icons'
 
+import { Background } from '../../components/Background';
+import { Heading } from '../../components/Heading';
+import { DuoCard } from '../../components/DuoCard';
+
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { THEME } from '../../theme';
 import { styles } from './styles';
 
+import { GameParams } from '../../@types/navigation';
+import { TouchableOpacity, View, Image } from 'react-native';
+
+
 export function Game() {
+  const route = useRoute();
+  const game = route.params as GameParams;
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <Background>
       <SafeAreaView style={styles.container}>
+        <View style={styles.header} >
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name='chevron-thin-left'
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+          <Image
+            source={logoImg}
+            style={styles.logo}
+          />
+          <View style={styles.right} />
+        </View>
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+        <Heading
+          title={game.title}
+          subtitle='Conecte-se e comece a jogar!' />
+
+        <DuoCard />
 
       </SafeAreaView>
     </Background>
